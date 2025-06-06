6a3e39250e748a558dae928e02ad63b2e3fa0280
Dimitrije Glibic <dimitrije.glibic1@gmail.com>
2025-06-07 01:44:00 +0200

    Add route generators

---
+++ core/generator/routes/routeGenerator.ts
@@ -0,0 +1,31 @@
+import { AppRouteMeta } from '../../types/AppMeta';
+import path from 'path';
+import { writeToFile } from '../utils';
+
+export async function generateRouteBindings(routes: AppRouteMeta[]) {
+  const outputDir = path.resolve('src/generated/routes');
+  const filePath = path.join(outputDir, 'index.ts');
+
+  const lines: string[] = [
+    `import { Router } from 'express';`,
+    ``,
+    `const router = Router();`,
+    ``
+  ];
+
+  for (const route of routes) {
+    const relativeImportPath = path
+      .relative(outputDir, route.filePath)
+      .replace(/\\/g, '/') // Windows-safe
+      .replace(/\.ts$/, ''); // Strip .ts
+
+    lines.push(
+      `router.${route.method.toLowerCase()}('${route.path}', require('${relativeImportPath}').route.${route.handlerProperty});`
+    );
+  }
+
+  lines.push(``, `export default router;`);
+
+  const fileContent = lines.join('\n');
+  await writeToFile(filePath, fileContent);
+}
